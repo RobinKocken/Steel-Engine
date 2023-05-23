43c4d56370a0dbad6407afd937a4170ad8a030e4
Xander Veninga <39034729+EliteForce1@users.noreply.github.com>
2023-05-23 13:18:24 +0200

    Scriptable Objects for each crop

--- Assets/Scripts/Stations/Farm/CropData.cs
+++ Assets/Scripts/Stations/Farm/CropData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Crop", menuName = "Crop", order = 2)]
+[CreateAssetMenu(fileName = "Crop", menuName = "CropData", order = 2)]
 public class CropData : ScriptableObject
 {
     public InventoryManager.ItemName type;
