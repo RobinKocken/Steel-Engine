97112cd9a345b89ae5a3b08d4b9f8719863bf505
Robin Kocken <robin.a.kocken@gmail.com>
2023-06-12 13:57:17 +0200

    stuff

---
+++ Assets/Scripts/Player/Stats/PlayerStatus.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public int health;
+    public int hunger;
+    public int thirst;
+}
